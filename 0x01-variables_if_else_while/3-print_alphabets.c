889828c85f2f446d0972038683e108cbc96f51ef
aminedraoui <draoui.aminee@gmail.com>
2022-07-02 08:18:18 -0700

    alph

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/*alphabet program*/
+int main (void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i;
+for (i = 0; i < 52; i++)
+{
+putchar(alpha[i]);
+}
+putchar('n\');
+return (0);
+}
